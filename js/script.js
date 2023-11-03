a6cdc4e9784910b9a2b811ebcc2387d9e4fc2b6a
Eron Moraes <eron.moraes7@gmail.com>
2023-11-03 10:12:57 -0300

    dados api ok

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
-const fetchPokemon = async (pokemon => {
+const pokemonName = document.querySelector('.pokemon__name');
+
+const fetchPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const data = await APIResponse.json();
+    return data;
+}
 
-console.log(APIResponse)
+const renderPokemon = async (pokemon) => {
+    const data = await fetchPokemon(pokemon);
 
-})
+    pokemonName.innerHTML = data.name;
 
-fetchPokemon();
+}
+
+renderPokemon('25')
