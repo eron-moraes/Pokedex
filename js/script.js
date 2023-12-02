53fd3cf57e65f33e82fbbf049b1804cfa6306fa4
Eron Moraes <eron.moraes7@gmail.com>
2023-12-02 08:16:53 -0300

    pokedex 23

--- js/script.js
+++ js/script.js
@@ -10,7 +10,13 @@ const renderPokemon = async (pokemon) => {
     const data = await fetchPokemon(pokemon);
 
     pokemonName.innerHTML = data.name;
-
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src = data['sprites']['version']['heneration-v']['black-white']['animated']['front_defalt'];
+    input.value = '';
 }
 
-renderPokemon('25')
+FormData.addEventListener('submit', (event) => {
+    event.preventDefalt();
+    renderPokemon(input.value);
+    
+})
